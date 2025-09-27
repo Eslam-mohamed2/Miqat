051cd74091ceb6e9f6cb2924a0f378f3858be61f
Eslam-Mohamed2 <em354219@gmail.com>
2025-09-27 23:06:14 +0300

    last update

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Home } from './home/home';
+import { Footer } from "./footer/footer";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header,FontAwesomeModule, Home],
+  imports: [RouterOutlet, Header, FontAwesomeModule, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
