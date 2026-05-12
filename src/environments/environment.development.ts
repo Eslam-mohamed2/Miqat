d53e97bba119f96b3cb7209128486f57561e806d
Eslam-Mohamed2 <em354219@gmail.com>
2026-05-12 23:54:12 +0300

    google Auth Fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://miqatsmartcalendar-b7e4anhxh8d5cmcx.israelcentral-01.azurewebsites.net',
-  googleClientId: '670515710919-o1roc1k3kvdoqkv2vd2r7i2i1ptdstm0.apps.googleusercontent.com'
+  googleClientId: '235553917648-mf5vor4qfd4c2l8ap61lk06gnvi36eam.apps.googleusercontent.com'
 };
